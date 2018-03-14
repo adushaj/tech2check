b74a4f7690e5e471f5187c33dfaac9b8bd8cc4f3
chrisdepalma <cndepalma@oakland.edu>
2018-03-14 14:32:57 -0400

    Specifications added

--- item_details.php
+++ item_details.php
@@ -34,6 +34,23 @@
   <link href="css/style.css" rel="stylesheet">
   <!-- Override CSS sheet -->
   <link href="css/Item_details.css" rel="stylesheet">
+  <style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even) {
+    background-color: #ededed;
+}
+</style>
 </head>
 
 <!-- ======== @Region: body ======== -->
@@ -54,7 +71,7 @@
       <div class="col-xs-12 col-md-4 col-lg-4">
         <?php
         $themodel = $_GET["model"];
-
+        $_SESSION['modelS'] = $themodel;
         $sql = "Select file_path from model where model_id = $themodel";
         $result = mysql_query($sql);
         $pic = mysql_fetch_array($result);
@@ -74,7 +91,7 @@
         <form action="push/reservepush.php" method="get">
         <div id="equipment-checkout">
           <?php
-            $ecount = "Select serial_number from equipment where model_id = $themodel";
+            $ecount = "SELECT serial_number FROM equipment WHERE model_id = $themodel AND status_id = 1";
             $eresult = mysql_query($ecount);
             
             $rcount = "SELECT reservation_id FROM reservation_list WHERE model_id = $themodel AND fulfilled_indicator = 0";
@@ -82,14 +99,18 @@
             
             $result = mysql_num_rows($eresult) - mysql_num_rows($rresult);
             
-            echo "<h4> Count: " . $result . "</h4>";
+            echo "<h2>" . $result . " Available</h2>";
+            
+            $findlength = "SELECT r.rental_length 
+              FROM rental_length r
+              JOIN type t ON r.rental_length_id = t.rental_length_id
+              JOIN model m ON t.type_id = m.type_id 
+              WHERE m.model_id = $themodel";
+            $fresult = mysql_fetch_array(mysql_query($findlength));
+            
+            echo "<h4>" . $fresult['rental_length'] . " day rental</h4>";
           
-            $avail = "select * from equipment_status
-            join equipment on equipment_status.status_id = equipment.status_id
-            where model_id = $themodel";
-            $result = mysql_query($avail);
-            $data = mysql_fetch_array($result);
-            echo "<h4> Status: " .$data['condition'] . "</h4>";
+
           ?>
           
           <input type="hidden" id="model" name="model" value="<?php echo $themodel; ?>">
@@ -98,11 +119,74 @@
       </div>
       </form>
     </div>
+    
+<!--item specs-->
+    <div class="row">
+      <div class="col-xs-12 col-md-6 col-lg-6">
+        <br>
+        <table>
+          <tr><th><b>Specifications</b></th></tr>
+        <?php
+        $specificationsList = "SELECT * FROM specifications 
+        JOIN model ON specifications.spec_id = model.spec_id
+        WHERE model.model_id = $themodel";
+        
+        $specs = mysql_query($specificationsList);
+        $row = mysql_fetch_array($specs);
+        
+        
+        if($row['processor'] != NULL){
+            echo "<tr><td><b>Processor<b></td>" . "<td>" . $row['processor'] . "</td></tr>";
+        }
+        if($row['ram'] != NULL){
+            echo "<tr><td><b>RAM</b></td>" . "<td>" . $row['ram'] . "</td></tr>";
+        }
+        if($row['storage'] != NULL){
+            echo "<tr><td><b>Storage</b></td>" . "<td>" . $row['storage'] . "</td></tr>";
+        }          
+        if($row['graphics'] != NULL){
+            echo "<tr><td><b>Graphics</b></td>" . "<td>" . $row['graphics'] . "</td></tr>";
+        } 
+        if($row['operating_system'] != NULL){
+            echo "<tr><td><b>Operating System</b></td>" . "<td>" . $row['operating_system'] . "</td></tr>";
+        }         
+        if($row['weight'] != NULL){
+            echo "<tr><td><b>Weight</b></td>" . "<td>" . $row['weight'] . " lb" . "</td></tr>";
+        }         
+        if($row['color'] != NULL){
+            echo "<tr><td><b>Color</b></td>" . "<td>" . $row['color'] . "</td></tr>";;
+        }         
+        if($row['port_type_1'] != NULL){
+            echo "<tr><td><b>Port 1</b></td>" . "<td>" . $row['port_type_1_qty'] . "x " . $row['port_type_1'] . "</td></tr>";
+        }         
+        if($row['port_type_2'] != NULL){
+            echo "<tr><td><b>Port 2</b></td>" . "<td>" . $row['port_type_2_qty'] . "x " . $row['port_type_2'] . "</td></tr>";
+        } 
+        if($row['port_type_3'] != NULL){
+            echo "<tr><td><b>Port 3</b></td>" . "<td>" . $row['port_type_3_qty'] . "x " . $row['port_type_3'] . "</td></tr>";
+        }           
+        if($row['screen_size'] != NULL){
+            echo "<tr><td><b>Screen Size</b></td>" . "<td>" . $row['screen_size'] . "</td></tr>";
+        } 
+        if($row['wireless_indicator'] != NULL){
+            echo "<tr><td><b>Wireless Indicator</b></td>" . "<td>" . $row['wireless_indicator'] . "</td></tr>";
+        }        
+        if($row['megapixels'] != NULL){
+            echo "<tr><td><b>Megapixels</b></td>" . "<td>" . $row['megapixels'] . "</td></tr>";
+        }
+        if($row['model_year'] != NULL){
+            echo "<tr><td><b>Model Year</b></td>" . "<td>" . $row['model_year'] . "</td></tr>";
+        }        
+    ?>
+      </table>
+      </div>
+    </div>
+    
   </div>
   </div>
+
         <div class="container">
             <div class="block">
-        <hr>
         <hr>
         <h4 class="block-title">
            Related Items
